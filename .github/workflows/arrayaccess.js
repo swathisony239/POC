c05531b3ead92dd1ab89c4a8d3835b30fe7883c1
swathisony239 <45678998+swathisony239@users.noreply.github.com>
2024-12-08 19:56:41 +0530

    Create arrayaccess.js

---
+++ .github/workflows/arrayaccess.js
@@ -0,0 +1,21 @@
+var items = [
+  {
+    "name":"orange"
+    "category":"fruit"
+    "price":"50"
+  },
+  {
+    "name":"Milk"
+    "category":"Dairy"
+    "price":"30"
+  },
+  {
+    "name":"carrot"
+    "category":"vegetable"
+    "price":"55"
+  }
+]
+
+document.getElementById(result1).innerHTML = items[0].name
+document.getElementById(result2).innerHTML = items[0].category
+document.getElementById(result3).innerHTML = items[0].price
